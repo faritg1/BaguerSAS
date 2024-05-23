55a7e4244b78ace946f1b0e600f3eed1d0c604e8
faritg1 <juanagelves@gmail.com>
2024-05-22 21:25:14 -0500

    feat: :sparkles: Implementando metodos de extension ratelimiting y config cors

--- Backend/Api/Program.cs
+++ Backend/Api/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using Api.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Data;
 
@@ -9,6 +11,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.ConfigureCors();
+builder.Services.AddApplicationServices();
+builder.Services.ConfigureRateLimiting();
+builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
 
 builder.Services.AddDbContext<BaguerContext>(options =>
 {
