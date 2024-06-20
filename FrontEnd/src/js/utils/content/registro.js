81d67d55af3e85307fafc8f150f9f730b423c981
faritg1 <juanagelves@gmail.com>
2024-06-19 20:45:46 -0500

    feat: :recycle: Refactorizando registro

--- FrontEnd/src/js/utils/content/registro.js
+++ FrontEnd/src/js/utils/content/registro.js
@@ -1,4 +1,3 @@
-import {tokenJWT} from '../auth/jwt.js';
 import {endpoint} from '../api/endpoints.js';
 import {request} from '../api/request.js';
 import {HTTP_METHOD_POST} from "../../const.js";
@@ -6,24 +5,24 @@ import {mostrarMensaje} from './mostrarMensaje.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const mensajeEstado = document.getElementById('mensajeEstado');
-    document.querySelector('.iniciar').addEventListener('submit', async (event) => {
+    document.querySelector('.registro').addEventListener('submit', async (event) => {
         event.preventDefault();
         const usernameValue = document.getElementById('user').value;
         const passwordValue = document.getElementById('pass').value;
         try{
             if(!((usernameValue === "") || (passwordValue === ""))) {
                 const loginUser = { username: usernameValue, password: passwordValue };
-                const response = await request(endpoint.loginUser, HTTP_METHOD_POST, loginUser);    
-                if(response.isAuthenticated === true) {
-                    tokenJWT.setToken({ Key: 'token', Value: `${response.token}` });
-                    mostrarMensaje('inicio de sesion exitoso', 'correcto');
-                    setTimeout(() => {
-                        window.location.href = "src/page/home.html";
-                    }, 1000); 
+                const response = await request(endpoint.registroUser, HTTP_METHOD_POST, loginUser);    
+                console.log(response);
+                if(response.statusCode === 400) {
+                    mostrarMensaje('Usuario ya registrado', 'error');
                 }else if(response.statusCode == 500){
                     mostrarMensaje('Problemas con el server', 'error');
                 }else{
-                    mostrarMensaje('Usuario o contraseña incorrecta', 'error');
+                    mostrarMensaje('Registro exitoso', 'correcto');
+                    setTimeout(() => {
+                        window.location.href = "../../index.html";
+                    }, 1000); 
                 }
             }
         }catch(e){
